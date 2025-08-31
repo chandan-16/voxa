01dba4b33b5af50a1e5965ce59975a748196c120
chandan-16 <chandangautam420.cg@gmail.com>
2025-08-31 20:08:57 +0530

    Creating sidebar of the the website, adding tailwind styles according to the state condition

---
+++ app/page.jsx
@@ -0,0 +1,44 @@
+'use client';
+import { useState } from "react";
+import Image from 'next/image';
+import { assets } from "@/assets/assets";
+import Sidebar from "@/components/Sidebar";
+
+export default function Home() {
+
+  const [expand, setExpand] = useState(false);
+  const [messages, setmessages] = useState([]);
+  const [loading, setIsLoading] = useState(false);
+
+
+  return (
+   <div>
+    <div className="flex h-screen ">
+      <Sidebar expand={expand} setExpand={setExpand} />
+
+      <div className="flex flex-1 flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
+        <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full ">
+          <Image onClick={() => setExpand(!expand)} src={assets.menu_icon} alt="menu" className='rotate-180'/>
+          <Image src={assets.chat_icon} alt="chat" className='opacity-70' />
+        </div>
+
+        {
+          messages.length === 0 ? (
+          <>
+          <div className="flex items-center gap-3">
+            <Image src={assets.logo_icon} alt='' className="h-16" />
+            <p className="text-2xl font-medium">Hi, I'm Voxa-AI.</p>
+          </div>
+          <p className="text-sm mt-2">How can I help you today?</p>
+          </>
+        ) : (<div></div>)
+        }
+
+        {/* propmt-box  */}
+
+        <p className="text-xs absolute bottom-1 text-gray-500">AI generated response, for reference purpose only.</p>
+      </div>
+    </div>
+   </div>
+  );
+}
